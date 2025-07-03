8083b724b17c894fa48f7f78bc09db7a0f1cae13
Samantha-Butler <butlersj1993@gmail.com>
2025-07-03 14:27:26 +0100

    Edited

--- obj/Debug/net9.0/CodingPractise.AssemblyInfo.cs
+++ obj/Debug/net9.0/CodingPractise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodingPractise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12262039bfe57d832ed13295804d8ca38163f90a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodingPractise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodingPractise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
